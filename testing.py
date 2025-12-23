35c39fec0740bbb16cf5f967260dbdbc4f9b8160
tandel <tandelarkan@gmail.com>
2025-12-23 20:37:55 +0530

    this is the original code that

---
+++ testing.py
@@ -0,0 +1,6 @@
+status = "success"   # change to "failed" and see output
+
+if status == "success":
+    print("🚀 Deployment completed successfully")
+else:
+    print("❌ Deployment failed. Please check logs")
